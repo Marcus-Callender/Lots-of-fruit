06ec00f748e74e1e92a822171ae4d03feea6af40
Marcus Callender <m.shute@outlook.com>
2017-02-01 14:18:23 +0000

    Created a loader objcet and added style to the loading screen in the .css file

--- Fruit wars/js/game.js
+++ Fruit wars/js/game.js
@@ -80,3 +80,76 @@ var levels =
 	}
 }
 
+var loader = 
+{
+	loaded : true,
+	assetsLoadedCount : 0,
+	totalassetsCount : 0,
+	
+	soundFileExtn : ".ogg",
+	
+	init : function()
+	{
+		// checks the compatibility with audio filetypes
+		var mpsSupport, oggSupport;
+		var audio = document.createElement('audio');
+		
+		if (audio.canPlayType)
+		{
+			// canPlayType will return "", "mayby" or "probably"
+			mp3Support = ("" != audio.canPlayType('audio/mpeg'));
+			oggSupport = ("" != audio.canPlayType('audio/ogg; codecs = "vorbis"'));
+		}
+		else
+		{
+			mp3Support = false;
+			oggSupport = false;
+		}
+		
+		// check for ogg and mp3 support, if nither are true set the type to undefined
+		loader.soundFileExtn = oggSupport ? ".ogg" : (mp3Support ? "mp3" : undefined);
+	},
+	
+	loadImage : function(url)
+	{
+		this.totalassetsCount++;
+		this.loaded = false;
+		$('#loadingscreen').show();
+		var image = new Image();
+		image.src = url;
+		image.onload = loader.itemLoaded;
+		return image;
+	},
+	
+	loadSound : function(url)
+	{
+		this.totalassetsCount++;
+		this.loaded = false;
+		$('#loadingscreen').show();
+		var audio = new Audio();
+		audio.src = url + loader.soundFileExtn;
+		audio.addEventListener("canplaythrough", loader.itemLoaded, false);
+		return audio;
+	},
+	
+	itemLoaded : function()
+	{
+		loader.assetsLoadedCount++;
+		$('#loadingmessage').html('Loaded ' + loader.assetsLoadedCount + ' of ' + loader.totalassetsCount);
+		
+		// if true all assets have been loaded
+		if (loader.loadedCount === loader.totalassetsCount)
+		{
+			loader.loaded = true;
+			$('#loadingscreen').hide();
+			
+			// if the loader.onload method has been assigned, run it then unasign it
+			if (loader.onload)
+			{
+				loader.onload();
+				loader.onload = undefined;
+			}
+		}
+	}
+}
+
