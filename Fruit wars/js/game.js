ac35ea8d651dac74935efde623ec41c1c8eb47fc
Marcus Callender <m.shute@outlook.com>
2017-02-04 17:28:17 +0000

    Created a list of entities and added placeholder functions

--- Fruit wars/js/game.js
+++ Fruit wars/js/game.js
@@ -40,6 +40,98 @@
 	}
 }());
 
+var entities =
+{
+	definitions :
+	{
+		"glass" :
+		{
+			fullHealth : 100,
+			density : 2.4,
+			friction : 0.4,
+			restitution : 0.15,
+		},
+		"wood" :
+		{
+			fullHealth : 500,
+			density : 0.7,
+			friction : 0.4,
+			restitution : 0.4,
+		},
+		"dirt" :
+		{
+			density : 3.0,
+			friction : 1.5,
+			restitution : 0.2,
+		},
+		"burger" :
+		{
+			shape : "circle",
+			radius : 25,
+			fullHealth : 40,
+			density : 1.0,
+			friction : 0.5,
+			restitution : 0.4,
+		},
+		"soda" :
+		{
+			shape : "rectangle",
+			width : 40,
+			height : 60,
+			fullHealth : 80,
+			density : 1.0,
+			friction : 0.5,
+			restitution : 0.7,
+		},
+		"fries" :
+		{
+			shape : "rectangle",
+			width : 40,
+			height : 50,
+			fullHealth : 50,
+			density : 1.0,
+			friction : 0.5,
+			restitution : 0.6,
+		},
+		"apple" :
+		{
+			shape : "circle",
+			radius : 25,
+			density : 1.5,
+			friction : 0.5,
+			restitution : 0.4,
+		},
+		"orange" :
+		{
+			shape : "circle",
+			radius : 25,
+			density : 1.5,
+			friction : 0.5,
+			restitution : 0.4,
+		},
+		"strawberry" :
+		{
+			shape : "circle",
+			radius : 15,
+			density : 2.0,
+			friction : 0.5,
+			restitution : 0.4,
+		}
+	},
+	
+	// create a new Box2D rigid body and add it to the current world
+	create : function(entity)
+	{
+		
+	},
+	
+	// draw the specified entity at the specified position and angle
+	draw : function(entity, position, angle)
+	{
+		
+	}
+}
+
 // calles game.init when the window has fully loaded
 $(window).load(
 	function()
