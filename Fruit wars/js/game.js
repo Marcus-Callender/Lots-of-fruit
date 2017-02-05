1a3ff2e9df9c2107d7607582d1371d0448c6cd46
Marcus Callender <m.shute@outlook.com>
2017-02-05 11:55:21 +0000

    Added data for the entities in the levels into the levels object

--- Fruit wars/js/game.js
+++ Fruit wars/js/game.js
@@ -453,13 +453,52 @@ var levels =
 		{
 			foreground : 'desert-foreground',
 			background : 'clouds-background',
-			entities : []
+			entities : 
+			[
+				{type : "ground", name : "dirt", x : 500, y : 440, width : 1000, height : 20, isStatic : true},
+				{type : "ground", name : "wood", x : 180, y : 390, width : 40, height : 80, isStatic : true},
+				
+				{type : "block", name : "wood", x : 520, y : 375, angle : 90, width : 100, height : 25},
+				{type : "block", name : "glass", x : 520, y : 275, angle : 90, width : 100, height : 25},
+				{type : "block", name : "wood", x : 620, y : 375, angle : 90, width : 100, height : 25},
+				{type : "block", name : "glass", x : 620, y : 275, angle : 90, width : 100, height : 25},
+				
+				{type : "villain", name : "burger", x : 520, y : 200, calories : 590},
+				{type : "villain", name : "fries", x : 620, y : 200, calories : 420},
+				
+				{type : "hero", name : "orange", x : 90, y : 410},
+				{type : "hero", name : "apple", x : 150, y : 410},
+			]
 		},
 		// seccond level
 		{
 			foreground : 'desert-foreground',
 			background : 'clouds-background',
-			entities : []
+			entities : 
+			[
+				{type : "ground", name : "dirt", x : 500, y : 440, width : 1000, height : 20, isStatic : true},
+				{type : "ground", name : "wood", x : 180, y : 390, width : 40, height : 80, isStatic : true},
+				
+				{type : "block", name : "wood", x : 820, y : 375, angle : 90, width : 100, height : 25},
+				{type : "block", name : "wood", x : 720, y : 375, angle : 90, width : 100, height : 25},
+				{type : "block", name : "wood", x : 620, y : 375, angle : 90, width : 100, height : 25},
+				
+				{type : "block", name : "glass", x : 670, y : 310, width : 100, height : 25},
+				{type : "block", name : "glass", x : 770, y : 310, width : 100, height : 25},
+				
+				{type : "block", name : "glass", x : 670, y : 248, angle : 90, width : 100, height : 25},
+				{type : "block", name : "glass", x : 770, y : 248, angle : 90, width : 100, height : 25},
+				
+				{type : "block", name : "wood", x : 720, y : 180, width : 100, height : 25},
+				
+				{type : "villain", name : "burger", x : 715, y : 160, calories : 590},
+				{type : "villain", name : "fries", x : 670, y : 400, calories : 420},
+				{type : "villain", name : "soda", x : 765, y : 395, calories : 150},
+				
+				{type : "hero", name : "strawberry", x : 40, y : 420},
+				{type : "hero", name : "orange", x : 90, y : 410},
+				{type : "hero", name : "apple", x : 150, y : 410},
+			]
 		}
 	],
 		
