10f71ba24f56d221c838688959c2a1a12ae80af4
Marcus Callender <m.shute@outlook.com>
2017-02-28 12:34:57 +0000

    Added map data for the first singleplayer map and added a mission screen gamelayer

---
+++ last colony/JavaScript/maps.js
@@ -0,0 +1,16 @@
+// stores data about all the maps in the game
+var maps = 
+{
+	"singlePlayer" : 
+	[
+		{
+			"name" : "Training Mission",
+			"briefing" : "Learn  the basics of last coleny",
+			
+			"mapImage" : "images/maps/level-one-debug-grid.png",
+			"startX" : 4,
+			"startY" : 4,
+		},
+	]
+};
+
