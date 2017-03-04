d960e85a03a48ec15f8ca645d7a7a8da6ac22ed0
Marcus Callender <m.shute@outlook.com>
2017-03-04 10:24:57 +0000

    Updated game object to handle redrawing the game background and prepare for animation implementation and camera panning

--- last colony/JavaScript/game.js
+++ last colony/JavaScript/game.js
@@ -7,6 +7,20 @@ $(window).load(
 
 var game = 
 {
+	// the map is made up of tiles of this size in pixels (20 x 20 px)
+	gridSize : 20,
+	
+	// if true the background has been moved and need to be redrawn at its new loaction
+	backgroundMoved : true,
+	
+	// A control loop that runs at a fixed period of time
+	// exicute animation code every 100 milisecons
+	animationTimeout : 100,
+	
+	// the ammount the camera has been moved
+	offsetX : 0,
+	offsetY : 0,
+	
 	init : function()
 	{
 		// preload necicery assets
@@ -14,6 +28,51 @@ var game =
 		
 		$('.gamelayer').hide();
 		$('#gamestartscreen').show();
+		
+		game.backgroundCanvas = document.getElementByID('gamebackgroundcanvas');
+		game.backgroundContext = game.backgroundCanvas.getContext('2d');
+		
+		game.foregroundCanvas = document.GetElementByID('gameforegroundcanvas');
+		game.foregroundContext = game.foregroundCanvas.getContext('2d');
+		
+		game.canvasWidth = game.backgroundCanvas.width;
+		game.canvasHeight = game.backgroundCanvas.height;
+	},
+	
+	start : function()
+	{
+		$('.gamelayer').hide();
+		$('gameinterfacescreen').show();
+		game.running = true;
+		game.refreshBackground = true;
+		
+		game.drawingLoop();
+	},
+	
+	animation : function()
+	{
+		// To be implemented
+	}
+	
+	drawingLoop : function()
+	{
+		// handle panning the camera
+		
+		// if the background needs to be refreshed draw the image at its new position then agnowlage it has been refreshed
+		// this reduces the workload of constantly redrawing the background
+		if (game.refreshBackground)
+		{
+			game.backgroundContext.drawImage(game.currentMapImage, game.offsetX, game.offsetY,
+				game.canvasWidth, game.canvasHeight, 0, 0, game.canvasWidth, game.canvasHeight);
+				
+			game.refreshBackground = false;
+		}
+		
+		if (game.running)
+		{
+			// call the drawing loop for the next frame using requestAnimationFrame
+			requestAnimationFrame(game.drawingLoop);
+		}
 	}
 }
 
